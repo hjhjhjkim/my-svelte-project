d4bdedd90a1df5511285d4fff0f8048796e69ca6
hjhjhjkim <71803211+hjhjhjkim@users.noreply.github.com>
2020-09-24 16:56:27 +0900

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,18 @@
+<script>
+  import first from "./first.svelte";
+  import second from "./second.svelte";
+</script>
+
+<style>
+  h1:first-child {
+    font-family: "Do Hyeon", sans-serif;
+  }
+  h1:last-child {
+    font-family: "Nanum Pen Script", cursive;
+  }
+</style>
+
+<h1>1번 폰트입니다.</h1>
+<h1>2번 폰트입니다.</h1>
+<h2>{first}</h2>
+<h2>{second}</h2>
