f82c48e2aff3062d604c1c545d8c9ffd0b6834b1
hjhjhjkim <71803211+hjhjhjkim@users.noreply.github.com>
2020-09-25 15:05:05 +0900

    tying-import(2)
    To be continued. . .

--- src/App.svelte
+++ src/App.svelte
@@ -2,6 +2,8 @@
   let text = "";
   let number = 0;
   import First from "./first.svelte";
+  import Second from "./second.svelte";
+  import Third from "./third.svelte";
 </script>
 
 <style>
@@ -12,8 +14,16 @@
   h1 {
     font-family: "Anton", sans-serif;
   }
-  /*font-family: "Nanum Myeongjo", serif;
-  font-family: "Nanum Pen Script", cursive;
+  h2 {
+    font-family: "Nanum Myeongjo", serif;
+  }
+  h3 {
+    font-family: "Nanum Pen Script", cursive;
+  }
+  input::placeholder {
+    font-family: "Anton", sans-serif;
+  }
+  /*
   font-family: "Open Sans Condensed", sans-serif;
   font-family: "Peddana", serif;
   font-family: "Ranchers", cursive;
@@ -23,7 +33,9 @@
 
 <div class="container">
   <section>
-    <h1>#6 InputBinding {First}</h1>
+    <h1>#6 InputBinding</h1>
+    <h2>inputbinding font2</h2>
+    <h3>input font3</h3>
 
     <label for="letter">text</label>
     <input id="letter" type="text" bind:value={text} />
