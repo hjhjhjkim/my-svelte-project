a1f09d23286a292547f67baf57fd97fec647d958
hjhjhjkim <71803211+hjhjhjkim@users.noreply.github.com>
2020-11-06 01:49:50 +0900

    timer

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,7 @@
 <script>
   var count = 0;
   $: console.log(count);
-  $: if (count >= 10) {
+  $: if (count >= 60) {
     alert("초과!");
     count = 0;
   }
@@ -9,18 +9,24 @@
     alert("!");
     count = 0;
   }
+
   var time = 0;
   function noodle() {
+    clearInterval(time);
     time = setInterval(timer(), 1000);
   }
   function timer() {
     count = count - 1;
-    document.getElementById("countdown").innerHTML = { count };
+    document.getElementById("countdown").innerHTML = count;
+    if (count == 0) {
+      clearInterval(time);
+      alert("Time Over");
+    }
   }
 </script>
 
 <body>
-  <p id="countdown">{count}</p>
+  <input id="countdown" value={count} />
   <button
     on:click={() => {
       count++;
