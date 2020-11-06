4fa315f834ed8803d3683dc804e57841ce944527
hjhjhjkim <71803211+hjhjhjkim@users.noreply.github.com>
2020-11-06 14:31:16 +0900

    timer

--- src/App.svelte
+++ src/App.svelte
@@ -9,10 +9,35 @@
     alert("!");
     count = 0;
   }
+
+  var time = 0;
+  var counts = 0;
+  function onclick() {
+    counts = parseInt(document.getElementById("countdown").value);
+    time = setInterval(timer(), 1000);
+  }
+  function timer() {
+    document.getElementById("countdown").innerHTML = counts;
+    counts = counts - 1;
+
+    if (counts == 0) {
+      clearInterval(time);
+      alert("Time Over");
+    }
+  }
 </script>
 
-<body>
-  <p id="countdown">{count}</p>
+<style>
+  .circle {
+    width: 100px;
+    height: 100px;
+    border: 3pt;
+    border-radius: 50px;
+  }
+</style>
+
+<body class="circle">
+  <input id="countdown" value={count} />
   <button
     on:click={() => {
       count++;
@@ -22,20 +47,5 @@
     on:click={() => {
       count--;
     }}>-</button>
-  <button onclick="noodle()">start!</button>
-  <script>
-    var time = 0;
-    function noodle() {
-      clearInterval(time);
-      time = setInterval(timer(), 1000);
-    }
-    function timer() {
-      document.getElementById("countdown").innerHTML = count = count - 1;
-
-      if (count == 0) {
-        clearInterval(time);
-        alert("Time Over");
-      }
-    }
-  </script>
+  <button onclick="onclick()">start!</button>
 </body>
